86440d56871b8385f81775581b4a81b671f5252d
Alvin Delagon <me@adelagon.com>
2023-02-28 07:38:41 +0000

    added a rust function

--- serverless/serverless/serverless_stack.py
+++ serverless/serverless/serverless_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
     Duration,
     Stack,
-    aws_sqs as sqs,
     aws_lambda as _lambda,
 )
 from constructs import Construct
@@ -11,18 +10,10 @@ class ServerlessStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-
-        # The code that defines your stack goes here
-
-        # example resource
-        queue = sqs.Queue(
-             self, "ServerlessQueue",
-             visibility_timeout=Duration.seconds(300),
+        f = _lambda.Function(self, "hello-rust",
+                runtime=_lambda.Runtime.PROVIDED_AL2,
+                handler="not.required",
+                code=_lambda.Code.from_asset(
+                    "lambda/hello/target/lambda/hello"
+                )
         )
-
-        # f = _lambda.Function(self, "hello-rust",
-        #         runtime=_lambda.Runtime.PROVIDED.AL2,
-        #         handler="not.required",
-        #         code=_lambda.Code.from_asset(
-                    
-        #         )
