82dc2642a33d0707f037c040c785831e6d7cf5a7
Alvin Delagon <me@adelagon.com>
2023-02-28 08:54:45 +0000

    Add APIGW

--- serverless/serverless/serverless_stack.py
+++ serverless/serverless/serverless_stack.py
@@ -19,7 +19,7 @@ class ServerlessStack(Stack):
                 )
         )
 
-        apigw.LambaRestApi(
+        apigw.LambdaRestApi(
             self, "APIEndpoint",
             handler=handler
         )
